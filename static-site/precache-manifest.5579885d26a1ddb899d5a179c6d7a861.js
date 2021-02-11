7460d41e52ba21ddb344e2a40e9a7d0c228f1a08
ken.collins <ken.collins@regeneron.com>
2021-02-11 15:12:30 -0500

    Added AvatarList and stories.

---
+++ static-site/precache-manifest.5579885d26a1ddb899d5a179c6d7a861.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbeb9205a3d98abf3fa26934157c3fbe",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.102cfc6f4fac94272601.bundle.js"
+  },
+  {
+    "url": "runtime~main.102cfc6f4fac94272601.bundle.js"
+  },
+  {
+    "url": "vendors~main.102cfc6f4fac94272601.bundle.js"
+  },
+  {
+    "url": "vendors~main.102cfc6f4fac94272601.bundle.js.LICENSE.txt"
+  }
+]);
